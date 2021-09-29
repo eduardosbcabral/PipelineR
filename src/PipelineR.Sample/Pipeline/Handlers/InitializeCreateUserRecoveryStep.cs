fa2b32332dbb097bf83080110909e245d00a2432
Eduardo Cabral <eduardosbcabral@gmail.com>
2021-09-29 18:20:46 -0300

    ✨ Adds a way to start the pipeline from step

---
+++ src/PipelineR.Sample/Pipeline/Handlers/InitializeCreateUserRecoveryStep.cs
@@ -0,0 +1,19 @@
+﻿namespace PipelineR.Sample.Pipeline.Handlers
+{
+    public class InitializeCreateUserRecoveryStep : RecoveryHandler<UserContext, UserRequest>, IInitializeCreateUserRecoveryStep
+    {
+        public InitializeCreateUserRecoveryStep(UserContext context) : base(context)
+        {
+        }
+
+        public override void HandleRecovery(UserRequest request)
+        {
+            this.Context.RecoveryWasExecuted = true;
+        }
+    }
+
+    public interface IInitializeCreateUserRecoveryStep : IRecoveryHandler<UserContext, UserRequest>
+    {
+
+    }
+}
